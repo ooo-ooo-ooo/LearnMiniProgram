a11353e54a5239032ed6771f95624dd062c66461
ltt <245343723@qq.com>
2019-11-21 13:22:12 +0800

    注册小程序和页面

--- app.js
+++ app.js
@@ -1,39 +1,49 @@
-//app.js
 App({
+  // 生命周期
+  // 小程序初始化完成时，会执行的生命周期（全局只触发一次）
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // console.log('小程序初始化完成：onLaunch')
+    // 获取用户信息(异步调用)
+    // wx.getUserInfo({
+    //   // 数据拿到之后，再进行回调
+    //   success: function(res){
+    //     console.log(res)
+    //   }
+    // })
+  },
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+  //  小程序显示出来时
+  onShow: function (options) {
+    // console.log('界面显示出来：onShow')
+    // console.log(options);
+    // 1.判断小程序的进入场景
+    // switch(options.scene){
+    //   case 1001:
+    //     break;
+    //   case 1005:
+    //     break;
+    // }
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
-      }
-    })
+
+    // 2.获取用户的信息，并且获取到用户信息之后，将用户的信息传递给服务器
+    // wx.getUserInfo({
+    //   success: function(res){
+    //     console.log(res);
+    //   }
+    // })  
+  },
+
+  //  小程序隐藏时
+  onHide: function () {
+    console.log('界面被隐藏时会执行：onHide')
+  },
+
+  // 小程序产生一些错误时
+  onError: function (msg) {
+    console.log('小程序中发生了一些错误时会执行：onError')
   },
   globalData: {
-    userInfo: null
+    name: "coderwhy",
+    age: 18
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,59 @@
 //index.js
 //获取应用实例
-const app = getApp()
+// getApp()获取App()产生的示例对象
+const app = getApp();
+const name = app.globalData.name;
+const age = app.globalData.age;
 
 Page({
+  // ------------- 2.初始化数据 ---------------------------
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+   
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  // ----------- 1.监听页面的生命周期函数 ---------------
+  // 页面被加载出来
+  onLoad(){
+    // console.log("onLoad")
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   箭头函数中的this一层层向上找
+    //   success: (res) => {
+    //     console.log(res);
+    //   }
+    // })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  // 页面显示出来时
+  onShow() {
+    console.log("onShow")
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  // 页面初次渲染时
+  onReady(){
+    console.log("onReady")
+  },
+  // 页面隐藏起来时
+  onHide(){
+    console.log("onHide")
+  },
+  onUnload(){
+    console.log("onUnload")
+  },
+
+  // ------------- 3. 监听wxml中相关的一些事件 --------------
+  handleGetUserInfo(event) {
+    console.log(event);
+  },
+
+  // ------------- 4. 监听其他事件 --------------
+  // 监听页面的滚动
+  onPageScroll(obj){
+    // console.log(obj)
+  },
+  // 监听页面滚动到底部
+  onReachBottom(){
+    console.log("页面滚动到底部")
+  },
+  // 下拉刷新事件
+  onPullDownRefresh(){
+    console.log("下拉刷新")
   }
 })

a5395bdfdd12b89a3e9d717309f4f245dcf6a33c
ltt <245343723@qq.com>
2019-11-28 11:39:20 +0800

    事件处理

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,64 +5,64 @@ Page({
    * 页面的初始数据
    */
   data: {
-    price: "25.66666"
+    price: "25.66666",
+    titles: ['衣服', '裤子', '鞋子']
   },
   handleBtnClick(){
     console.log("按钮发生点击")
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleTouchStart(){
+    console.log("handleTouchStart")
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleTouchMove() {
+    console.log("handleTouchMove")
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleTouchEnd() {
+    console.log("handleTouchEnd")
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleTap() {
+    console.log("handleTap")
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  handleLongpress() {
+    console.log("handleLongpress")
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
+  handleEventClick(event){
+    console.log('------',event)
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
+  handleEventEnd(event){
+    console.log('+++++++', event)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleInner(event){
+    console.log(event)
+  },
+  handleOuter(event){
+    console.log(event)
+  },
+  handleItemClick(event){
+    console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title, index)
+  },
+ 
+  // --------------------- 事件冒泡和事件捕获 ---------------
+  handleCaptureView1(){
+    console.log("handleCaptureView1")
+  },
+  handleBindView1(){
+    console.log("handleBindView1")
+  },
+  handleCaptureView2() {
+    console.log("handleCaptureView2")
+  },
+  handleBindView2() {
+    console.log("handleBindView2")
+  },
+  handleCaptureView3() {
+    console.log("handleCaptureView3")
+  },
+  handleBindView3() {
+    console.log("handleBindView3")
   }
 })

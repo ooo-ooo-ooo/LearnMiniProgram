1c976c8045a25abeedd2f5e1341a35cae34af049
ltt <245343723@qq.com>
2019-12-11 09:48:47 +0800

    弹窗和分享

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,52 +3,49 @@
 const app = getApp()
 
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中',
+      mask: true,
+      icon: "loading",
+      duration: 3000
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈哈',
+      // showCancel: false,
+      cancelText: "退出",
+      cancelColor: "red",
+      success: function(res){
+        if(res.cancel){
+          console.log("用户点击了取消按钮")
         }
-      })
-    }
+        if(res.confirm){
+          console.log("用户点击了确认按钮")
+        }
+      }
+    })
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
+
+    setTimeout(() => {
+      // 必须手动hideLoading才会让loading消失
+      wx.hideLoading()
+    },2000)
+  },
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ["相册", "拍照"],
+      itemColor: 'red',
+      success: function(res){
+        console.log(res)
+      }
     })
   }
 })
